f02890e86d20deb36bc8c4392461ea1095600fb6
Hugo Meneses <hugo.meneses@govic.cl>
2015-02-03 18:53:31 -0300

    Se agrega directiva de publicaciones con datos dummy desde controller.
    [#LUF-17]

--- www/app/newsfeed/newsfeed.controller.js
+++ www/app/newsfeed/newsfeed.controller.js
@@ -1,5 +1,61 @@
 angular.module('lufke')
-	.controller('NewsfeedCtrl', function ($scope) {
-		console.log('Inicia ... NewsfeedCtrl');
-		$scope.dato_prueba = 'DATOS DE PRUEBA CONTROLADOR NEWSFEED';
-	});
+        .controller('NewsfeedCtrl', function ($scope) {
+            console.log('Inicia ... NewsfeedCtrl');
+            $scope.dato_prueba = 'DATOS DE PRUEBA CONTROLADOR NEWSFEED';
+
+            var dummy =
+                    [{
+                            background: 'http://goo.gl/56Q265',
+                            profile: {
+                                img: 'http://goo.gl/qBdA7s',
+                                name: 'Elber Galarga'
+                            },
+                            location: 'Pichilemu',
+                            time: '4h',
+                            text: 'Hola mundo',
+                            stars: 2,
+                            comments: 5,
+                            link: ''
+                        },
+                        {
+                            background: 'http://goo.gl/56Q265',
+                            profile: {
+                                img: 'http://goo.gl/qBdA7s',
+                                name: 'Elber Galarga'
+                            },
+                            location: 'Pichilemu',
+                            time: '4h',
+                            text: 'Hola mundo',
+                            stars: 2,
+                            comments: 5,
+                            link: ''
+                        },
+                        {
+                            background: 'http://goo.gl/56Q265',
+                            profile: {
+                                img: 'http://goo.gl/qBdA7s',
+                                name: 'Elber Galarga'
+                            },
+                            location: 'Pichilemu',
+                            time: '4h',
+                            text: 'Hola mundo',
+                            stars: 2,
+                            comments: 5,
+                            link: ''
+                        },
+                        {
+                            background: 'http://goo.gl/56Q265',
+                            profile: {
+                                img: 'http://goo.gl/qBdA7s',
+                                name: 'Elber Galarga'
+                            },
+                            location: 'Pichilemu',
+                            time: '4h',
+                            text: 'Hola mundo',
+                            stars: 2,
+                            comments: 5,
+                            link: ''
+                        }];
+
+            $scope.datos = dummy;
+        });
