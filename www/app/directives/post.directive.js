2f2e1fe8d748ae4722456cb99c28b23debe49c92
Hugo Meneses <hugo.meneses@govic.cl>
2015-02-11 11:51:44 -0300

    Orden de nombres de archivo, mejora nomenclatura.

--- www/app/directives/post.directive.js
+++ www/app/directives/post.directive.js
@@ -2,6 +2,6 @@ angular.module('lufke')
 		.directive('lufkePost', function () {
 			return {
 				restrict: 'E',
-				templateUrl: 'app/directives/post.html'
+				templateUrl: 'app/templates/post-summary.html'
 			};
 		});
--- www/app/services/session.js
+++
@@ -1,8 +0,0 @@
-angular.module('lufke')
-		.factory('SesionService', function ($localStorage) {
-			return {
-				isLoggedIn: function () {
-					return false;
-				}
-			};
-		});
