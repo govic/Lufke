c545f12c9d29367e62875efad88dfd6a9fdd61d4
Hugo Meneses <hugo.meneses@govic.cl>
2015-02-13 16:38:42 -0300

    Se agrega post detallado, mejoras de interfaz y mejora de dummies.
    Se agregan comentarios (con clic largo salen opciones) y también la vista detallada de imagen con acciones y comentarios.
    
    [#LUF-18]

--- www/app/controllers/post.controller.js
+++ www/app/controllers/post.controller.js
@@ -1,6 +1,70 @@
 angular.module('lufke')
-		.controller('PostCtrl', function ($scope, $stateParams, PostsService) {
+		.controller('PostCtrl', function ($scope, $stateParams, $ionicPopup, PostsService) {
 			console.log('Inicia ... PostCtrl');
 
 			$scope.post = PostsService.getPost($stateParams.postId);
+
+			$scope.updatePost = function () {
+				$scope.$broadcast('scroll.refreshComplete');
+				$scope.post = PostsService.getPost($stateParams.postId);
+				//TODO: agregar comentarios nuevos o algo asi
+			};
+
+			var optionsPopup;
+
+			$scope.showMore = function () {
+				optionsPopup = $ionicPopup.show({
+					templateUrl: 'app/templates/post-more.html',
+					title: 'Acciones',
+					scope: $scope,
+					buttons: [
+						{
+							text: 'Cancelar'
+						}
+					]
+				});
+
+				optionsPopup.then(function (res) {
+					console.log('Tapped!', res);
+				});
+			};
+
+			$scope.share = function () {
+				optionsPopup.close();
+				alert("share");
+
+			};
+
+			$scope.report = function () {
+				optionsPopup.close();
+				alert("report");
+			};
+
+			$scope.showCommentOptions = function () {
+				var confirmPopup = $ionicPopup.confirm({
+					title: 'Borrar comentario',
+					template: '¿Está seguro que desea borrar este comentario?',
+					buttons: [{
+							text: 'No',
+							type: 'button-default',
+							onTap: function (e) {
+								// e.preventDefault() will stop the popup from closing when tapped.
+							}
+						}, {
+							text: 'Sí',
+							type: 'button-positive',
+							onTap: function (e) {
+								// Returning a value will cause the promise to resolve with the given value.
+								//return scope.data.response;
+							}
+						}]
+				});
+				confirmPopup.then(function (res) {
+					if (res) {
+						console.log('You are sure');
+					} else {
+						console.log('You are not sure');
+					}
+				});
+			};
 		});
---
+++ www/app/dummy/posts.dummy.js
@@ -0,0 +1,385 @@
+var dummyPostList = [
+	{
+		"postId": 1,
+		"backgroundImgUrl": "http://goo.gl/56Q265",
+		"profile": {
+			"image": "http://goo.gl/PXFWyj",
+			"name": "Soila Cuevas"
+		},
+		"location": "Pichilemu",
+		"timestamp": "4h",
+		"text": "Tengo lindos ojos vieron?",
+		"totalStars": 42,
+		"totalComments": 3,
+		"link": ""
+	},
+	{
+		"postId": 2,
+		"backgroundImgUrl": "http://i.imgur.com/xxyIUPy.jpg",
+		"profile": {
+			"image": "http://goo.gl/9kbu0C",
+			"name": "Elsa Popeludo"
+		},
+		"location": "Viña del Mar",
+		"timestamp": "6h",
+		"text": "Hola mundo",
+		"totalStars": 2,
+		"totalComments": 5,
+		"link": ""
+	},
+	{
+		"postId": 6,
+		"backgroundImgUrl": "http://goo.gl/jMSO58",
+		"profile": {
+			"image": "http://i.imgur.com/WDFrSmC.png",
+			"name": "Cuño Chileno"
+		},
+		"location": "Chillán",
+		"timestamp": "8h",
+		"text": "En el trekking para a la cabaña 25!",
+		"totalStars": 0,
+		"totalComments": 2,
+		"link": ""
+	},
+	{
+		"postId": 3,
+		"backgroundImgUrl": "http://goo.gl/wgyCuG",
+		"profile": {
+			"image": "http://i.imgur.com/dC0xDm4.png",
+			"name": "Coneja Dientuda"
+		},
+		"location": "Lago Todos los Santos",
+		"timestamp": "30m",
+		"text": "Hola mundo",
+		"totalStars": 2,
+		"totalComments": 5,
+		"link": ""
+	},
+	{
+		"postId": 4,
+		"backgroundImgUrl": "http://goo.gl/oLqDKT",
+		"profile": {
+			"image": "http://goo.gl/syLmKX",
+			"name": "Johnny Sins"
+		},
+		"location": "Pichilemu",
+		"timestamp": "4h",
+		"text": "Respeto por la amistad y la pureza!",
+		"totalStars": 2,
+		"totalComments": 5,
+		"link": ""
+	},
+	{
+		"postId": 5,
+		"backgroundImgUrl": "http://goo.gl/6G0uaG",
+		"profile": {
+			"image": "http://goo.gl/gZQkgx",
+			"name": "Feastancia Pérez"
+		},
+		"location": "Arica",
+		"timestamp": "8h",
+		"text": "Ahí con las chiquillas bailando en el agüita jijiji",
+		"totalStars": 0,
+		"totalComments": 2,
+		"link": ""
+	}
+];
+
+
+var dummyPostDetail = [
+	{
+		"postId": 1,
+		"backgroundImgUrl": "http://goo.gl/56Q265",
+		"profile": {
+			"image": "http://goo.gl/PXFWyj",
+			"name": "Soila Cuevas"
+		},
+		"location": "Pichilemu",
+		"timestamp": "4h",
+		"text": "Tengo lindos ojos vieron?",
+		"totalStars": 42,
+		"totalComments": 3,
+		"link": "",
+		"comments": [
+			{
+				"commentId": "54dcdc1cabff9284fc32e01a",
+				"profile": {
+					"name": "Mullins Dominguez",
+					"image": "http://placehold.it/48x48"
+				},
+				"timestramp": "0m",
+				"text": "Enim incididunt veniam dolore officia laboris nostrud."
+			},
+			{
+				"commentId": "54dcdc1cad07d034ae2448cc",
+				"profile": {
+					"name": "Estes Frazier",
+					"image": "http://placehold.it/48x48"
+				},
+				"timestramp": "1m",
+				"text": "Ex ut aliqua ullamco sint."
+			},
+			{
+				"commentId": "54dcdc1c88638591ea145947",
+				"profile": {
+					"name": "Dean Ward",
+					"image": "http://placehold.it/48x48"
+				},
+				"timestramp": "2m",
+				"text": "Eu ipsum veniam officia non deserunt est dolore Lorem labore."
+			}
+		]
+	},
+	{
+		"postId": 2,
+		"backgroundImgUrl": "http://i.imgur.com/xxyIUPy.jpg",
+		"profile": {
+			"image": "http://goo.gl/9kbu0C",
+			"name": "Elsa Popeludo"
+		},
+		"location": "Viña del Mar",
+		"timestamp": "6h",
+		"text": "Hola mundo",
+		"totalStars": 2,
+		"totalComments": 5,
+		"link": "",
+		"comments": [
+			{
+				"commentId": "54dcdc571730aae4f1434e34",
+				"profile": {
+					"name": "Emma Clarke",
+					"image": "http://placehold.it/48x48"
+				},
+				"timestramp": "0m",
+				"text": "Magna cupidatat ea anim mollit."
+			},
+			{
+				"commentId": "54dcdc585ea78943be8f24b4",
+				"profile": {
+					"name": "Saunders Rasmussen",
+					"image": "http://placehold.it/48x48"
+				},
+				"timestramp": "1m",
+				"text": "Ea id excepteur aliqua mollit occaecat occaecat non esse."
+			},
+			{
+				"commentId": "54dcdc5859de299944f1d824",
+				"profile": {
+					"name": "Langley Ruiz",
+					"image": "http://placehold.it/48x48"
+				},
+				"timestramp": "2m",
+				"text": "Occaecat consectetur ullamco aliqua non nisi pariatur voluptate Lorem et labore."
+			},
+			{
+				"commentId": "54dcdc5872c90cb94d0fb076",
+				"profile": {
+					"name": "Hunter Hewitt",
+					"image": "http://placehold.it/48x48"
+				},
+				"timestramp": "3m",
+				"text": "Cillum et deserunt aliquip nisi sint magna ipsum commodo mollit."
+			},
+			{
+				"commentId": "54dcdc58005e677cabef96f7",
+				"profile": {
+					"name": "Pate Hughes",
+					"image": "http://placehold.it/48x48"
+				},
+				"timestramp": "4m",
+				"text": "Non proident officia eiusmod esse culpa Lorem eu nulla."
+			}
+		]
+	},
+	{
+		"postId": 6,
+		"backgroundImgUrl": "http://goo.gl/jMSO58",
+		"profile": {
+			"image": "http://i.imgur.com/WDFrSmC.png",
+			"name": "Cuño Chileno"
+		},
+		"location": "Chillán",
+		"timestamp": "8h",
+		"text": "En el trekking para a la cabaña 25!",
+		"totalStars": 0,
+		"totalComments": 2,
+		"link": "",
+		"comments": [
+			{
+				"commentId": "54dcdc8c50f536dcbc46e0f3",
+				"profile": {
+					"name": "Katheryn Delaney",
+					"image": "http://placehold.it/48x48"
+				},
+				"timestramp": "0m",
+				"text": "Exercitation anim fugiat et aute magna quis ut occaecat laborum laboris consequat."
+			},
+			{
+				"commentId": "54dcdc8c0096a278cd71e7e9",
+				"profile": {
+					"name": "Albert Mccall",
+					"image": "http://placehold.it/48x48"
+				},
+				"timestramp": "1m",
+				"text": "Fugiat irure reprehenderit exercitation sit in."
+			}
+		]
+	},
+	{
+		"postId": 3,
+		"backgroundImgUrl": "http://goo.gl/wgyCuG",
+		"profile": {
+			"image": "http://i.imgur.com/dC0xDm4.png",
+			"name": "Coneja Dientuda"
+		},
+		"location": "Lago Todos los Santos",
+		"timestamp": "30m",
+		"text": "Hola mundo",
+		"totalStars": 2,
+		"totalComments": 5,
+		"link": "",
+		"comments": [
+			{
+				"commentId": "54dcdcb2c9ce59a3739bf2b8",
+				"profile": {
+					"name": "Rutledge Hess",
+					"image": "http://placehold.it/48x48"
+				},
+				"timestramp": "0m",
+				"text": "Eu excepteur fugiat eiusmod cillum mollit id amet veniam consectetur."
+			},
+			{
+				"commentId": "54dcdcb2979eab13b249495c",
+				"profile": {
+					"name": "Matthews Paul",
+					"image": "http://placehold.it/48x48"
+				},
+				"timestramp": "1m",
+				"text": "Id anim proident incididunt veniam consequat elit in."
+			},
+			{
+				"commentId": "54dcdcb21c773004865e2827",
+				"profile": {
+					"name": "Malinda Stephenson",
+					"image": "http://placehold.it/48x48"
+				},
+				"timestramp": "2m",
+				"text": "Aute do ad sint reprehenderit ex amet minim."
+			},
+			{
+				"commentId": "54dcdcb2a6b4f1f2c0c255c6",
+				"profile": {
+					"name": "Mason Dotson",
+					"image": "http://placehold.it/48x48"
+				},
+				"timestramp": "3m",
+				"text": "Cupidatat cillum nisi fugiat et deserunt duis irure anim quis veniam labore reprehenderit voluptate."
+			},
+			{
+				"commentId": "54dcdcb2553693de446b7bfd",
+				"profile": {
+					"name": "Smith Kennedy",
+					"image": "http://placehold.it/48x48"
+				},
+				"timestramp": "4m",
+				"text": "Aliqua nisi in sunt aute voluptate ipsum eu occaecat et ipsum reprehenderit fugiat magna."
+			}
+		]
+	},
+	{
+		"postId": 4,
+		"backgroundImgUrl": "http://goo.gl/oLqDKT",
+		"profile": {
+			"image": "http://goo.gl/syLmKX",
+			"name": "Johnny Sins"
+		},
+		"location": "Pichilemu",
+		"timestamp": "4h",
+		"text": "Respeto por la amistad y la pureza!",
+		"totalStars": 2,
+		"totalComments": 5,
+		"link": "",
+		"comments":
+				[
+					{
+						"commentId": "54dcdcc7f6817605e39a23f5",
+						"profile": {
+							"name": "Edwards Wade",
+							"image": "http://placehold.it/48x48"
+						},
+						"timestramp": "0m",
+						"text": "Fugiat cillum nulla incididunt deserunt non reprehenderit duis. Fugiat cillum nulla incididunt deserunt non reprehenderit duis. Fugiat cillum nulla incididunt deserunt non reprehenderit duis. Fugiat cillum nulla incididunt deserunt non reprehenderit duis."
+					},
+					{
+						"commentId": "54dcdcc78fe1a1a3ae5e983f",
+						"profile": {
+							"name": "Mari Foley",
+							"image": "http://placehold.it/48x48"
+						},
+						"timestramp": "1m",
+						"text": "Deserunt enim nulla amet consectetur veniam esse anim pariatur do ipsum."
+					},
+					{
+						"commentId": "54dcdcc763061eaa213e6a47",
+						"profile": {
+							"name": "Moore Fowler",
+							"image": "http://placehold.it/48x48"
+						},
+						"timestramp": "2m",
+						"text": "Sit aute duis consequat ullamco dolor aliquip ex."
+					},
+					{
+						"commentId": "54dcdcc7e9df7d45c50af211",
+						"profile": {
+							"name": "Acevedo Lamb",
+							"image": "http://placehold.it/48x48"
+						},
+						"timestramp": "3m",
+						"text": "Excepteur tempor adipisicing non pariatur ullamco laborum proident nulla laboris velit qui."
+					},
+					{
+						"commentId": "54dcdcc7a3d636452f2c02dc",
+						"profile": {
+							"name": "Osborn Leblanc",
+							"image": "http://placehold.it/48x48"
+						},
+						"timestramp": "4m",
+						"text": "Exercitation esse proident eu deserunt commodo commodo elit aliquip esse aliqua incididunt."
+					}
+				]
+	},
+	{
+		"postId": 5,
+		"backgroundImgUrl": "http://goo.gl/6G0uaG",
+		"profile": {
+			"image": "http://goo.gl/gZQkgx",
+			"name": "Feastancia Pérez"
+		},
+		"location": "Arica",
+		"timestamp": "8h",
+		"text": "Ahí con las chiquillas bailando en el agüita jijiji",
+		"totalStars": 0,
+		"totalComments": 2,
+		"link": "",
+		"comments": [
+			{
+				"commentId": "54dcdcdb381a10041925f2c9",
+				"profile": {
+					"name": "Marva Hensley",
+					"image": "http://placehold.it/48x48"
+				},
+				"timestramp": "0m",
+				"text": "Occaecat qui fugiat qui mollit nisi sit culpa ea non."
+			},
+			{
+				"commentId": "54dcdcdbb48dd730650e1f10",
+				"profile": {
+					"name": "Alta Duke",
+					"image": "http://placehold.it/48x48"
+				},
+				"timestramp": "1m",
+				"text": "Cupidatat officia consectetur fugiat reprehenderit aliquip exercitation mollit amet ullamco dolor ipsum."
+			}
+		]
+	}
+];
